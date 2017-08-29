fe25e1d5b35fb21fe9879e2b587e4c2a622fc3a2
Engen <enggenapp@gmail.com>
2017-08-29 13:15:58 +0100

    checked

--- domain.py
+++ domain.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlparse
 
 
-# Get domain name (example.com)
+# Get domain name (example.com).
 def get_domain_name(url):
     try:
         results = get_sub_domain_name(url).split('.')
@@ -10,7 +10,7 @@ def get_domain_name(url):
         return ''
 
 
-# Get sub domain name (name.example.com)
+# Get sub domain name (name.example.com).
 def get_sub_domain_name(url):
     try:
         return urlparse(url).netloc
